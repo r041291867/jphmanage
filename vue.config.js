7cbc0a768dd3ff5fa569b24a6139fb92bbe017d9
r041291867 <r041291687@gmail.com>
2018-09-11 14:20:28 +0800

    modify icon route

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,28 @@ function resolve (dir) {
 
 module.exports = {
   baseUrl: "./",
+  /*css : {
+    // extract CSS in components into a single CSS file (only in production)
+    // can also be an object of options to pass to extract-text-webpack-plugin
+    extract: true,
+
+    // Enable CSS modules for all css / pre-processor files.
+    // This option does not affect *.vue files.
+    modules: true,
+
+    // enable CSS source maps?
+    sourceMap: false,
+
+    // pass custom options to pre-processor loaders. e.g. to pass options to
+    // sass-loader, use { sass: { ... } }
+    loaderOptions: {
+      baseUrl:"../../",
+      sass:{
+        css: 'css-loader',
+        'scss':'css-loader | sass-loader'
+      }
+    }
+  },*/
   devServer: {
     proxy: {
       "/api": {
@@ -32,5 +54,5 @@ module.exports = {
       .set('layout',resolve('src/layout'))
       .set('base',resolve('src/base'))
       .set('static',resolve('src/static'))
-   }
+   },
 }
